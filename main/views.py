679e32b357d0c829c1c4fd1f7529a5011b26b2ce
ik <herrkarstone@gmail.com>
2024-02-08 20:00:10 +0300

    Main app is ready, pages home and about

--- main/views.py
+++ main/views.py
@@ -4,12 +4,16 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Главная страница магазина - Home',
-        'list': ['first', 'second']
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
     }
     return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return HttpResponse('About Page')
+    context = {
+            'title': 'Home - О нас',
+            'content': 'О нас',
+            'text_on_page': 'Текст о том почему этот магазин такой классный'
+        }
+    return render(request, 'main/about.html', context)
