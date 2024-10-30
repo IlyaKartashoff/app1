0d6d337429835572ddeeb10a02f5158883a15bf7
Ilya Kartashov <106881356+IlyaKartashoff@users.noreply.github.com>
2024-10-30 16:26:33 +0300

    Update models.py

--- goods/models.py
+++ goods/models.py
@@ -1,11 +1,6 @@
 import decimal
 from email.mime import image
 from email.policy import default
-from enum import unique
-from tabnanny import verbose
-from tkinter import CASCADE
-from unicodedata import category
-from unittest.util import _MAX_LENGTH
 from django.db import models
 
 class Categories(models.Model):
